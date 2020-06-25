631a8750193fd46188b1c67813eae9529afc7bff
Dan Lynch <pyramation@gmail.com>
2020-06-24 22:25:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tests 
+
+https://github.com/bitcoinjs/bitcoinjs-message/issues/24
+
+You'll see 2 directories, testv4 and testv5 which use the different version of the bitcoinjs lib
---
+++ testv5/__tests__/index.test.js
@@ -0,0 +1,16 @@
+const bitcoin = require("bitcoinjs-lib"); // v5.x.x
+const bitcoinMessage = require("bitcoinjs-message");
+
+describe(
+  'straight from the docs', () => {
+    const keyPair = bitcoin.ECPair.fromWIF('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1')
+    var privateKey = keyPair.privateKey
+    var message = 'This is an example of a signed message.'
+    
+    var signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed)
+    console.log(signature.toString('base64'))
+    // => 'G9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk='
+    expect(signature.toString('base64')).toEqual(
+      'G9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk=')
+  }
+)
